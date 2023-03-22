f51811038b600edee8e97dbf35f9a7fafd499256
Niharika Pandey <niharikapandeyabc@gmail.com>
2023-03-22 11:47:03 +0530

    port

--- script.js
+++ script.js
@@ -65,12 +65,12 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings:  ["Java Backend Developer!", "Problem  Solver","Fast Learner"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-2", {
+    //     strings:  ["Java Backend Developer!", "Problem  Solver","Fast Learner"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     // owl carousel script
     $('.carousel').owlCarousel({
